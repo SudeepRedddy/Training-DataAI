02822b4d7a4422d86dada2ae2ef69dfb4cf8d472
Sudeep Reddy <reddysudeep196@gmail.com>
2026-02-09 09:51:21 +0530

    day1 2

---
+++ DAY12/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+@app.route("/about")
+def about():
+    return "About Page!"
+
+@app.route("/about/<name>")
+def user(name):
+    return f"Hello,{name}"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
